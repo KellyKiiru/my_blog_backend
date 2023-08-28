683023c0404e29ed5779dcb2d467bc33a97488fc
KellyKiiru <infowithkiiru@gmail.com>
2023-08-28 11:49:33 +0300

    add article_title field to serialzer and create function

--- blog_db/serializers.py
+++ blog_db/serializers.py
@@ -6,9 +6,14 @@ class BlogsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = [
+            'article_title',
             'article',
             'image1',
             'image2',
             'image3',
             'date'
         ]
+
+    def create(self, validated_data):
+
+        return Blog.objects.create(**validated_data)
