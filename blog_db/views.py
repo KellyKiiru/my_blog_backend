7ef32802b1db29cf5353778820daf37f1f73c514
KellyKiiru <infowithkiiru@gmail.com>
2023-08-28 11:51:03 +0300

    define APIView class with get method to get blog model instances

--- blog_db/views.py
+++ blog_db/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework import status
+
+from .models import *
+from .serializers import *
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("Blog is working")
+
+class GetBlogs(APIView):
+
+    def get(self, request, format=None):
+        blogs = Blog.objects.all()
+        serializer = BlogsSerializer(blogs, many=True)
+        return Response(serializer.data)
