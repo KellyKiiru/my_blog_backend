a8a9ffb60996b4a8e21deda86d476ab94a4799fe
KellyKiiru <infowithkiiru@gmail.com>
2023-08-28 11:49:58 +0300

    define urls to display blogs

--- blog_db/urls.py
+++ blog_db/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('blogs/',GetBlogs.as_view(), name='blogs')
 ]
