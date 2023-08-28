49a12f809e1726b600373698b1d984d1677c91f5
KellyKiiru <infowithkiiru@gmail.com>
2023-08-28 11:48:48 +0300

    add article_title field to blog model

--- blog_db/models.py
+++ blog_db/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class Blog(models.Model):
+    article_title= models.CharField(max_length=100, null=False, default="Heading")
     article = models.TextField()
     image1 = models.ImageField(null=True, blank=True)
     image2 = models.ImageField(null=True, blank=True)
     image3 = models.ImageField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return f'{self.article_title} - Article'
+    
